eae5af9ed792247173bc2c9d9bc1df2b172017fa
Nico <nicobdx@icloud.com>
2023-11-20 11:28:04 +0100

    Initial Commit

---
+++ Vibe/VibeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibeApp.swift
+//  Vibe
+//
+//  Created by Nicolas Bordeaux on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct VibeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
